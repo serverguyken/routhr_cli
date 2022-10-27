894a5ec5e95863c8663864f8613d221a7aaf0e40
serverguyken <serverguyken@gmail.com>
2022-10-27 17:48:09 -0500

    changed template

--- template/api/route/handler/product/get.ts
+++ template/api/route/handler/product/get.ts
@@ -11,7 +11,7 @@ const prodHandler = async (req: RequestInterface, res: ResponseInterface) => {
     const prodID = req.params.prodID as string;
     const service = new ProductsService();
     const product = await service.FindProductById(prodID);
-    const status = createStatus(200, 0, `Product with id ${prodID} found.`);
+    const status = createStatus(200, 0, product ? `Product with id ${prodID} found.` : `Product with id ${prodID} not found.`);
     const response = createResponse(status, product);
     res.status(200).json(response);
 };
