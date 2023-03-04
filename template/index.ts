9f6fa37a636147c8e056dfc2ebefcc41b47cea46
Serverguyken <serverguyken@gmail.com>
2023-03-04 07:49:01 -0600

    updated app name to routhr

--- template/index.ts
+++ template/index.ts
@@ -3,11 +3,11 @@ const port: any = process.env.PORT || 3002;
 import cors from 'cors';
 const app = new Routhr();
 setEnv();
-app.use(cors());
-app.use(app.middleware.bodyParser.json);
+routhr.use(cors());
+routhr.use(routhr.middleware.bodyParser.json);
 import routes from './api/route';
 import { setEnv } from './config';
-app.useRoutes(routes);
-app.start(port, () => {
+routhr.useRoutes(routes);
+routhr.start(port, () => {
     console.log(`Server started on port ${port}`);
 });
