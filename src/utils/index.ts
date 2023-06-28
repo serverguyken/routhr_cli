407eda882856a512537addd3725f720a1e39e481
Serverguyken <serverguyken@gmail.com>
2023-06-28 12:28:22 -0400

    updated routhr version

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -57,7 +57,7 @@ export const createPkg = (name: string, description: string) => {
         author: "",
         license: "ISC",
         dependencies: {
-            routhr: "^2.0.0",
+            routhr: "^2.0.1",
             typescript: "^4.6.3",
             cors: "^2.8.5",
             axios: "^1.3.4",
